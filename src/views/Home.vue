d1a143313ba00beadde5c8741f541e94e402b779
Trefu_ <tre.lopez@hotmail.com>
2022-07-22 14:57:00 -0300

    l3 completado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events for good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 130139,
+          category: 'animal welfare',
+          title: 'cat day',
+          description: 'lorem impusn',
+          location: 'meow town',
+          date: 'Juanary 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'kat layden'
+        },
+        {
+          id: 9088,
+          category: 'animal ionoij',
+          title: 'cat month',
+          description: 'lorem impusn',
+          location: 'meow momomo',
+          date: 'Enero 1998',
+          time: '17:00',
+          petsAllowed: false,
+          organizer: 'kat layden'
+        },
+        {
+          id: 89898,
+          category: 'lkjkljklj welfare',
+          title: 'dog day',
+          description: 'lorem impusn',
+          location: 'meow town',
+          date: 'Febrero 2022',
+          time: '9:00',
+          petsAllowed: true,
+          organizer: 'kjklj jkjkj'
+        }
+      ]
+    }
   }
-};
+}
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
